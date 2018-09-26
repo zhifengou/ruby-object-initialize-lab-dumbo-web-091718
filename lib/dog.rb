a0e859473ffddf48be0994f893891c716e908437
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 13:11:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,7 @@ class Dog
     @name=name
     @breed=breed
   end
+<<<<<<< HEAD
 
   def name=(name)
     @name=name
@@ -16,7 +17,27 @@ class Dog
     @breed=breed
   end
 
+=======
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def breed=(breed)
+    @breed=breed
+  end
+  
+>>>>>>> 4163e2827bb920aed3097ec4fb6116e227c20328
   def breed
     @breed
   end
 end
+<<<<<<< HEAD
+=======
+
+   
+>>>>>>> 4163e2827bb920aed3097ec4fb6116e227c20328
--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,13 @@ class Person
     @name=name
   end
 
+<<<<<<< HEAD
   def name
     @name=name
   end
 end
+=======
+  # def name
+  #   @name=name
+end
+>>>>>>> 4163e2827bb920aed3097ec4fb6116e227c20328
